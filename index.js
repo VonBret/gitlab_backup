cb7d49c169be4d474d2a3134a7160c659b7dabd0
Bret_Von_XiaoMi <qdqx.von@foxmail.com>
2021-03-03 02:27:59 +0800

    代码上传

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 var dotenv = require("dotenv"); // .env 文件支撑
 dotenv.config();
 
-var directory = require('./lib/directory');
-var fetch = require('./lib/fetch');
+var directory = require("./lib/directory");
+var fetch = require("./lib/fetch");
 
 // 入口函数
-async function main () {
+async function von_gcb() {
     await directory.delFilesFolders();
     await fetch.write();
 }
-  
-main();
+
+von_gcb();
